7355628d338c703d2ef33fc183743bff602fbfdc
AryanAg08 <aryangoyal90@outlook.com>
2023-05-16 14:33:42 +0530

    Initial commit

--- application/src/App.js
+++ application/src/App.js
@@ -5,17 +5,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="https://www.propelleraero.com/wp-content/uploads/2021/05/Vector.svg" className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Task one!!
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://www.propelleraero.com/dirtmate/"
+          href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          propelleraero
+          Learn React
         </a>
       </header>
     </div>
